da8789930644658b7a6d5501b6b5f177f9faab41
Rob <samiamspop@gmail.com>
2016-03-31 18:58:06 -0700

    add contact, about, index and firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ask-anything',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ask-anything.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
